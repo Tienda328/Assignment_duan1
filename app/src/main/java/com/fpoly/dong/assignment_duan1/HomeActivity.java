e3f04b20430d8e37d610c6a48c9aaea80e7cc3d2
Tien Dat <datnguyentien328@gmail.com>
2018-11-21 23:07:18 +0700

    new uer

--- app/src/main/java/com/fpoly/dong/assignment_duan1/HomeActivity.java
+++ app/src/main/java/com/fpoly/dong/assignment_duan1/HomeActivity.java
@@ -1,21 +1,13 @@
 package com.fpoly.dong.assignment_duan1;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 
-public class HomeActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
+public class HomeActivity extends AppCompatActivity {
     private ImageView anhve;
     private ImageView new1;
     private ImageView squad;
@@ -26,9 +18,6 @@ public class HomeActivity extends AppCompatActivity  implements NavigationView.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         anhve = (ImageView) findViewById(R.id.anhve);
         new1 = (ImageView) findViewById(R.id.new1);
         squad = (ImageView) findViewById(R.id.squad);
@@ -54,70 +43,7 @@ public class HomeActivity extends AppCompatActivity  implements NavigationView.O
                 startActivity(intent3);
             }
         });
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
     }
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.home) {
-            // Handle the camera action
-        } else if (id == R.id.tintuc) {
-
-        } else if (id == R.id.banve) {
-
-        } else if (id == R.id.thanhvien) {
-
-        } else if (id == R.id.nguoidung) {
-
-        } else if (id == R.id.gioithieu) {
-
-        } else if (id == R.id.dangxuat) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return false;
-    }
 }
